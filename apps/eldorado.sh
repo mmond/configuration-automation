049ab5bbd3a6f6f4c15d6b9b49c6e743ef953701
mark <mmond@io.com>
2009-01-16 12:09:51 -0600

    rm extra prompt for cap passwd

--- apps/eldorado.sh
+++ apps/eldorado.sh
@@ -26,5 +26,4 @@ cat ../configuration-automation/config/eldorado.vhost |sed "s/TARGET_SERVER/$TAR
 cp ../configuration-automation/config/eldorado.database.yml config/database.yml
 
 #	Use Capistrano to configure directory structure, Eldorado and servers
-echo "Please enter the target server password again for Capistrano's El Dorado deployment"
 cap deploy:setup deploy:update deploy:symlink_vhost deploy:upload_conf_files deploy:chown_web deploy:remove_htaccess rake:db_create rake:db_schema_load rake:db_migrate passenger:restart
