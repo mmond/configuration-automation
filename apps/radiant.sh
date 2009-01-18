4b55acf71d5b2b5fd3e33050cfb90901efec6b75
mark <mmond@io.com>
2009-01-18 15:14:52 -0600

    3rd attempt to commit Radiant's chown_web rake task

--- apps/radiant.sh
+++ apps/radiant.sh
@@ -33,4 +33,4 @@ cat ../configuration-automation/config/radiant.vhost |sed "s/TARGET_SERVER/$TARG
 cp ../configuration-automation/config/radiant.database.yml config/database.yml
 
 #	Use Capistrano to configure directory structure, Radiant and servers
-cap deploy:setup deploy:update deploy:upload_conf_files deploy:symlink_vhost deploy:create_db rake:db_bootstrap passenger:restart
+cap deploy:setup deploy:update deploy:upload_conf_files deploy:symlink_vhost deploy:chown_web deploy:create_db rake:db_bootstrap passenger:restart
