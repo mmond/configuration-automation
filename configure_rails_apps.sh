46f748cd5729ef317c589ad5e6f48f6ccf166d27
mark <mmond@io.com>
2009-01-18 13:29:16 -0600

    call cap chown_web for Radiant install

--- configure_rails_apps.sh
+++ configure_rails_apps.sh
@@ -5,19 +5,28 @@
 echo "
 To install a production Rails application, choose from the list or [CTRL-C] to quit
 The choices are: "
+echo "0. None"
 echo "1. Radiant CMS"
-echo "2. El Dorado Community Web App"
-printf "Default (1): " ; read RAILS_APPLICATION
+echo "2. El Dorado"
+echo "3. Spree"
+printf "Default (0): " ; read RAILS_APPLICATION
 
 case $RAILS_APPLICATION in
-"", 1)
+0)
+	echo $RAILS_APPLICATION: Ok. Enjoy your server! >&2
+	exit 1
+	;;
+1)
 	source apps/radiant.sh
 	;;
 2)
 	source apps/eldorado.sh
 	;;
+3)
+	source apps/spree.sh
+	;;
 *)
-	echo $RAILS_APPLICATION: unknown option >&2
+	echo $RAILS_APPLICATION: That is an unknown option. Exiting... >&2
 	exit 1
 	;;
 esac
