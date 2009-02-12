f58aedd91c1bd39a51af171aa7f5a6dbefdc1b65
mark <mmond@io.com>
2009-02-12 13:14:26 -0600

    use same dir for app deploy instead of peer, since we are no longer downloading a local copy of each app

--- apps/radiant.sh
+++ apps/radiant.sh
@@ -16,8 +16,8 @@ fi
 #	download Radiant from Github to ~/radiant 
 echo "In a moment Capistrano will request your password.
 "
-mkdir -p ../radiant/config
-cd ../radiant/
+mkdir -p radiant/config
+cd radiant/
 capify .
 
 
@@ -36,4 +36,4 @@ cp ../configuration-automation/config/radiant.database.yml config/database.yml
 cap deploy:setup deploy:update deploy:upload_conf_files deploy:symlink_vhost deploy:chown_web deploy:create_db rake:db_bootstrap passenger:restart
 
 #	Go back to configuration-automation root dir (to allow all app installs)
-cd ../configuration-automation
+cd ..
--- apps/spree.sh
+++ apps/spree.sh
@@ -11,8 +11,8 @@ fi
 #	download Spree from Github to ~/spree 
 echo "In a moment Capistrano will request your password.
 "
-mkdir -p ../spree/config
-cd ../spree/
+mkdir -p spree/config
+cd spree/
 capify .
 
 #	Use configuration-automation's spree deploy.rb
@@ -30,4 +30,4 @@ cp ../configuration-automation/config/spree.database.yml config/database.yml
 cap deploy:setup deploy:update deploy:upload_conf_files deploy:symlink_vhost deploy:gems_install deploy:chown_web deploy:create_db rake:db_bootstrap passenger:restart
 
 #	Go back to configuration-automation root dir (to allow all app installs)
-cd ../configuration-automation
+cd ..
