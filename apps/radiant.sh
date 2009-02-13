c336e1a56c1f58688a7d23defec8f9cda8221f7f
mark <mmond@io.com>
2009-02-12 21:19:28 -0600

    more dir idiocy reversion

--- apps/radiant.sh
+++ apps/radiant.sh
@@ -23,14 +23,14 @@ capify .
 
 #	Use configuration-automation's Radiant deploy.rb
 #	Update the TARGET_SERVER placeholder in deploy.rb
-cat config/radiant.deploy.rb |sed "s/TARGET_SERVER/$TARGET_SERVER/g" > radiant/config/deploy.rb
+cat ../config/radiant.deploy.rb |sed "s/TARGET_SERVER/$TARGET_SERVER/g" > radiant/config/deploy.rb
 
 #	Use configuration-automation's Radiant vhost
 #	Update the TARGET_SERVER placeholder in the Apache vhost
-cat config/radiant.vhost |sed "s/TARGET_SERVER/$TARGET_SERVER/g" > radiant/config/radiant.vhost
+cat ../config/radiant.vhost |sed "s/TARGET_SERVER/$TARGET_SERVER/g" > radiant/config/radiant.vhost
 
 #	Use configuration-automation's Radiant database.yml
-cp config/radiant.database.yml radiant/config/database.yml
+cp ../config/radiant.database.yml radiant/config/database.yml
 
 #	Use Capistrano to configure directory structure, Radiant and servers
 cap deploy:setup deploy:update deploy:upload_conf_files deploy:symlink_vhost deploy:chown_web deploy:create_db rake:db_bootstrap passenger:restart
