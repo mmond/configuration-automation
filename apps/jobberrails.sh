f81379d9543ca4b46540f7728eb3f8347c0b9b36
mark <mmond@io.com>
2009-02-12 18:56:28 -0600

    clean up new dir structure bugs

--- apps/jobberrails.sh
+++ apps/jobberrails.sh
@@ -18,14 +18,14 @@ capify .
 
 #	Use configuration-automation's jobberRails deploy.rb
 #	Update the TARGET_SERVER placeholder in deploy.rb
-cat ../configuration-automation/config/jobberrails.deploy.rb |sed "s/TARGET_SERVER/$TARGET_SERVER/g" > config/deploy.rb
+cat config/jobberrails.deploy.rb |sed "s/TARGET_SERVER/$TARGET_SERVER/g" > jobberrails/config/deploy.rb
 
 #	Use configuration-automation's jobberRails vhost
 #	Update the TARGET_SERVER placeholder in the Apache vhost
-cat ../configuration-automation/config/jobberrails.vhost |sed "s/TARGET_SERVER/$TARGET_SERVER/g" > config/jobberrails.vhost
+cat config/jobberrails.vhost |sed "s/TARGET_SERVER/$TARGET_SERVER/g" > jobberrails/config/jobberrails.vhost
 
 #	Use configuration-automation's jobberRails database.yml
-cp ../configuration-automation/config/jobberrails.database.yml config/database.yml
+cp config/jobberrails.database.yml jobberrails/config/database.yml
 
 #	Use Capistrano to configure directory structure, jobberRails and servers
 cap deploy:setup deploy:update deploy:upload_conf_files deploy:symlink_vhost deploy:haml_install deploy:rails_install rake:gems_install deploy:chown_web rake:db_create rake:db_schema_load rake:db_migrate passenger:restart
